5011e2a0544586d7bf486e819c9442f1cdc8e382
vpopi4 <80127175+vpopi4@users.noreply.github.com>
2021-03-06 13:14:01 +0300

    Add files via upload

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pole = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.up = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pole
+            // 
+            this.pole.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pole.Location = new System.Drawing.Point(0, 0);
+            this.pole.Margin = new System.Windows.Forms.Padding(0);
+            this.pole.Name = "pole";
+            this.pole.Size = new System.Drawing.Size(650, 400);
+            this.pole.TabIndex = 0;
+            this.pole.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 400);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(650, 50);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(820, 50);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(709, 291);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(50, 50);
+            this.up.TabIndex = 2;
+            this.up.Text = "up";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(653, 347);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(50, 50);
+            this.left.TabIndex = 3;
+            this.left.Text = "left";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(709, 347);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(50, 50);
+            this.down.TabIndex = 4;
+            this.down.Text = "down";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(765, 347);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(50, 50);
+            this.right.TabIndex = 5;
+            this.right.Text = "right";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 451);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pole);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(840, 490);
+            this.MinimumSize = new System.Drawing.Size(840, 490);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Snake";
+            ((System.ComponentModel.ISupportInitialize)(this.pole)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pole;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ snake/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake
+{
+    
+    public partial class Form1 : Form
+    {
+        Snake snake;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.snake = new Snake();
+            timer1.Start();
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void down_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class Snake
+    {
+        public Snake()
+        {
+            this.header = new System.Windows.Forms.PictureBox();
+            this.body.Add(new System.Windows.Forms.PictureBox());
+        }
+
+        void addBody()
+        {
+            this.body.Add(new System.Windows.Forms.PictureBox());
+        }
+
+        void move(string direction)
+        {
+            switch (direction)
+            {
+                case "up":
+                    Point newPoint = new Point(header.Location.X, header.Location.Y - 10);
+                    header.Location = newPoint;
+
+                    foreach (System.Windows.Forms.PictureBox i in body)
+                    {
+                        i.Location = moveArr.Peek();
+                    }
+                    break;
+            }
+
+        }
+
+        System.Windows.Forms.PictureBox header;
+        List<System.Windows.Forms.PictureBox> body;
+        Queue<Point> moveArr;
+
+    }
+
+}
